bdc8fabab6d9845e075c872fe9096468687f7006
Sathya K <sathya.k.interns@metamation.com>
2023-07-28 09:39:13 +0530

    Reverting previous merge

--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         int n, k;
-         n = new Random ().Next (1, 100);
-         Console.Write ("Enter value for k:");
-         for (int i = 1; i < 9; i++) {
-            k = int.Parse (Console.ReadLine ());
-            if (n < k)
-               Console.WriteLine ("you guess is too high");
-            else if (n > k)
-               Console.WriteLine ("you guess is too low");
-            else {
-               Console.WriteLine ("you guessed correctly");
-               break;
-            }
-         }
+         Console.WriteLine ("Hello World!");
       }
    }
 }
