1037f37542b5c496b00d90677013efaf446a6de1
Sathya K <sathya.k.interns@metamation.com>
2023-07-28 12:07:07 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         int n, k;
+         n = new Random ().Next (1, 100);
+          for (; ; ) {
+            Console.Write ("Enter value for k:"); 
+            bool var = int.TryParse (Console.ReadLine (),out k );
+            if (var == false)
+               Console.WriteLine ("invalid number");
+            else {
+               if (n < k)
+                  Console.WriteLine ("you guess is too high");
+               else if (n > k)
+                  Console.WriteLine ("you guess is too low");
+               else {
+                  Console.WriteLine ("you guessed correctly");
+                  break;
+               }
+            }
+         }
       }
    }
 }
