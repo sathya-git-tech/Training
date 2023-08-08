63851507897d02165938d9c869a92d30d8e2d96e
Sathya K <sathya.k.interns@metamation.com>
2023-08-08 15:46:52 +0530

    Assignment T03

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+          
+         Console.Write ("Enter a number n:");
+         int n = int.Parse (Console.ReadLine ());
+         int a = 0;
+         int b = 1;
+         int c = a + b;
+         for(int i = 2; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+         }
+         Console.WriteLine ($"Fabionacci of {n} is {c}"); 
       }
    }
 }
