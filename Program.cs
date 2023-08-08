3b18567135e1bce176d9755662f541d5ea13bd7b
Sathya K <sathya.k.interns@metamation.com>
2023-08-08 17:06:47 +0530

    Assignment T05 done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.Write ("Enter a number:");
+         int n = int.Parse (Console.ReadLine ());
+         int result = 0;
+         for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+               result = 1;
+               break;
+            }
+         }
+         if (result == 0) {
+            Console.WriteLine ($"{n} is Prime");
+         } else {
+            Console.WriteLine ($"{n} is not prime");
+         }
       }
    }
 }
