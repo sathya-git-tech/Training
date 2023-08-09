b6c8f47a4ef4fd1e2121be32e13d7215ee6c555e
Sathya K <sathya.k.interns@metamation.com>
2023-08-09 17:49:46 +0530

    Assignment T06

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("Enter a name:");
+         string name = Console.ReadLine ();
+         Console.WriteLine ("Name is:" + name);
+         for (int i = (name.Length - 1); i >=0; i--) {
+            Console.WriteLine (name[i]);
+         }
+         Console.ReadKey ();
       }
    }
 }
