18d3a6e2bbcde2cf2d4f76a7979079f0ea01a904
Sathya K <sathya.k.interns@metamation.com>
2023-08-11 14:01:00 +0530

    Assignment T06 Palindrome

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         string inputstr, reversestr = string.Empty;
+         Console.Write ("Enter a string : ");
+         inputstr = Console.ReadLine ();
+           for (int i = inputstr.Length - 1; i >= 0; i--) {
+                 reversestr += inputstr[i];
+           }
+           if (reversestr == inputstr) {
+               Console.WriteLine ("String is Palindrome");
+           } else {
+               Console.WriteLine ("String is not Palindrome");
+           }
       }
    }
 }
