d32b5b42fcfb5dac35be0bef38f55d46a5217e15
Sathya K <sathya.k.interns@metamation.com>
2023-08-14 10:45:14 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿namespace Training {
+﻿using System;
+namespace Training {
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter a number:");
          int n = int.Parse (Console.ReadLine ());
-         int result = 0;
          for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-               result = 1;
+               Console.WriteLine ($"{n} is not Prime");
+               i = i + 1;
+               break;
+            } if(n % i == 1) {
+               Console.WriteLine ($"{n} is Prime");
+               i = i + 1;
                break;
             }
-         }
-         if (result == 0) {
-            Console.WriteLine ($"{n} is Prime");
-         } else {
-            Console.WriteLine ($"{n} is not prime");
          }
       }
    }
