16b2973bedfbee7a4aa2775f910f5d6bd365507a
Sathya K <sathya.k.interns@metamation.com>
2023-08-14 12:49:53 +0530

    Prime Number

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 namespace Training {
    internal class Program {
-      static void Main (string[] args) {
-         Console.Write ("Enter a number:");
-         int n = int.Parse (Console.ReadLine ());
+      static void Main (string[] args) { 
+         Console.Write ("Enter a number n:");
+        int n = int.Parse (Console.ReadLine ());
+         bool prime = true;
          for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-               Console.WriteLine ($"{n} is not Prime");
-               i = i + 1;
-               break;
-            } if(n % i == 1) {
-               Console.WriteLine ($"{n} is Prime");
-               i = i + 1;
+               Console.WriteLine ("is not prime");
+               prime = false;
                break;
             }
+         }
+         if (prime == true) {
+            Console.WriteLine ("is prime");
          }
       }
    }
