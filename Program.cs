466dc444e4231d262c9a5a934a93f1f2031a176c
Sathya K <sathya.k.interns@metamation.com>
2023-08-17 12:47:21 +0530

    Prime number T05 changes done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 namespace Training {
    internal class Program {
-      static void Main (string[] args) { 
+      static void Main (string[] args) {
          Console.Write ("Enter a number n:");
-        int n = int.Parse (Console.ReadLine ());
+         int n = int.Parse (Console.ReadLine ());
          bool prime = true;
-         for (int i = 2; i < n; i++) {
+         for (int i = 2; i < n/2; i++) {
             if (n % i == 0) {
                Console.WriteLine ("is not prime");
                prime = false;
