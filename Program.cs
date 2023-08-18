2d42418089cdbaca23bc19dfacff764c80ea46e7
Sathya K <sathya.k.interns@metamation.com>
2023-08-18 14:34:11 +0530

    Reverse number program

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("Enter a number:");
+         int n = int.Parse (Console.ReadLine ());
+         int remainder = 0;
+         while(n>0) {
+            remainder = ((remainder * 10) + n % 10);
+            n /= 10;
+         }
+         Console.WriteLine ($"{remainder}");
       }
    }
 }
