500647bed74375f683a0e0ad48e6b92111aad00d
Sathya K <sathya.k.interns@metamation.com>
2023-08-18 12:44:19 +0530

    Multiplication Tables

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         for(int i = 1; i <= 10; i++) {
+            Console.WriteLine (3 + "*" + i + "=" + (i * 3));
+         }
       }
    }
 }
