d42cd85f555b43f69f125a5ba05ab20d004ba633
Sathya K <sathya.k.interns@metamation.com>
2023-08-21 16:58:46 +0530

    Pascal's Triangle

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         int rows, space,a;
+         Console.Write ("Enter an odd number:");
+         rows = int.Parse (Console.ReadLine ());
+         
+         a = 1;
+         for (int i = 0; i <= rows; i++) {
+            for (space = 1; space <= rows - i; space++) {
+               Console.Write ("  ");
+            }
+            for (int j = 0; j <=i; j++) {
+               if (j == 0) {
+                   a = 1;
+               } 
+               else {
+                  a = a * (i - j + 1) / j;
+               }
+               Console.Write ($"{a,-3} ");
+            }
+            Console.Write ("\n");
+         }
       }
    }
 }
