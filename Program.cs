b462bd0957cb57e70ae55d7edb9d0bc734774a3b
Sathya K <sathya.k.interns@metamation.com>
2023-08-22 10:46:37 +0530

    Digital Root

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("Enter a number:");
+         int x = int.Parse (Console.ReadLine ());
+         int total = 0;
+
+         while (x > 0) {
+            total = total + (x % 10);
+            x /= 10;
+         }
+         Console.WriteLine ($"Digital root is: {total}");
       }
    }
 }
