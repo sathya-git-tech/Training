326db059d2d999a0f69768f931721961a6e69fc2
Sathya K <sathya.k.interns@metamation.com>
2023-08-22 09:52:09 +0530

    Print a Diamond

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         int rows, space,totalrows;
-         Console.Write ("Enter an odd number:");
-         totalrows  = int.Parse (Console.ReadLine ());
-         rows = totalrows / 2 + 1;
+         int rows, space;
+         Console.Write ("Enter a number:");
+         rows  = int.Parse (Console.ReadLine ());
 
-         for (int i = 1; i <= rows; i++) {
+         for (int i = 0; i <= rows; i++) {
             for (space = 1; space <= rows - i; space++) {
                Console.Write ("  ");
             }
-            for (int j = 1; j <= 2 * i - 1; j++) {
+            for (int j = 0; j <=i; j++) {
+               Console.Write ("* ");
+            }
+            for (int j = 1; j <= i; j++) {
                Console.Write ("* ");
             }
             Console.Write ("\n");
          }
          space = 1;
-         for (int i = rows - 1; i >= 1; i--) {
+         for (int i = rows - 1; i >= 0; i--) {
             for ( space = 1; space <= rows-i; space++) {
                Console.Write ("  ");
             }
-            for (int j = 1; j <= 2*i-1; j++) {
+            for (int j = 0; j <=i; j++) {
+               Console.Write ("* ");
+            }
+            for (int j = 1; j <= i; j++) {
                Console.Write ("* ");
             }
             Console.Write ("\n");
