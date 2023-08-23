76bc14367d9ae89f9762a97ecc47f9e240579450
Sathya K <sathya.k.interns@metamation.com>
2023-08-23 14:34:11 +0530

    changes done Assignment T09

--- Program.cs
+++ Program.cs
@@ -5,30 +5,39 @@
          Console.Write ("Enter a number:");
          rows  = int.Parse (Console.ReadLine ());
 
+         // build upper half of the diamond pattern
+
          for (int i = 0; i <= rows; i++) {
+            string pattern = string.Empty;
             for (space = 1; space <= rows - i; space++) {
-               Console.Write ("  ");
+               pattern += "  ";
             }
             for (int j = 0; j <=i; j++) {
-               Console.Write ("* ");
+               pattern += "* ";
             }
             for (int j = 1; j <= i; j++) {
-               Console.Write ("* ");
+               pattern += "* ";
             }
-            Console.Write ("\n");
+            Console.Write ($"\n{pattern}");
          }
+
+
+
+         // build lower half of the diamond pattern
+
          space = 1;
          for (int i = rows - 1; i >= 0; i--) {
+            string pattern = string.Empty;
             for ( space = 1; space <= rows-i; space++) {
-               Console.Write ("  ");
+               pattern += "  ";
             }
             for (int j = 0; j <=i; j++) {
-               Console.Write ("* ");
+               pattern += "* ";
             }
             for (int j = 1; j <= i; j++) {
-               Console.Write ("* ");
+               pattern += "* ";
             }
-            Console.Write ("\n");
+            Console.Write ($"\n{pattern}");
          }
       }
    }
