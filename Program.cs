eb9b2fdb5774e35ebf22ba20d8313b20f99955d3
Sathya K <sathya.k.interns@metamation.com>
2023-08-28 12:27:57 +0530

    Strong Password Assignment T13

--- Program.cs
+++ Program.cs
@@ -1,7 +1,51 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("Enter a password:");
+         string password = Console.ReadLine ();
+
+
+         bool Length = password.Length >= 6;
+         bool Digit = password.Any (char.IsDigit);
+         bool Lower = password.Any (char.IsLower);
+         bool Upper = password.Any (char.IsUpper);
+         bool Specialchar = password.Any (char.IsPunctuation);
+
+         foreach (char i in password ) {  
+               if (char.IsDigit (i)) {
+                  Digit = true;
+               }
+               if (char.IsLower (i)) {
+                  Lower = true;
+               }
+               if (char.IsUpper (i)) {
+                  Upper = true;
+               }
+               if ("!@#$%^&*()-+".Contains (i)) {
+                  Specialchar = true;
+               }
+         }
+         bool var = Length && Digit && Lower && Upper && Specialchar;
+         if (var == true) {
+            Console.WriteLine ("password is strong");
+         } else {
+            Console.WriteLine ("password is not strong");
+            if (Length == false) {
+               Console.WriteLine ("password should be at least 6 character");
+            }
+            if (Digit == false) {
+               Console.WriteLine ("password should contain at least one digit");
+            }
+            if(Lower == false) {
+               Console.WriteLine ("password should contain at least one Lowercase English character");
+            }
+            if(Upper ==false) {
+               Console.WriteLine ("password should contain at least one Uppercase English character");
+            }
+            if(Specialchar == false) {
+               Console.WriteLine ("password should contain at least one Special character");
+            }
+         }
       }
    }
 }
