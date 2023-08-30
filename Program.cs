d9708fd595669cb12903b4ba16fded309b978212
Sathya K <sathya.k.interns@metamation.com>
2023-08-30 10:08:20 +0530

    changes done T09

--- Program.cs
+++ Program.cs
@@ -1,43 +1,14 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         int rows, space;
-         Console.Write ("Enter a number:");
-         rows  = int.Parse (Console.ReadLine ());
+         Console.WriteLine ("Enter a number:");
+         int n = int.Parse (Console.ReadLine ());
 
-         // build upper half of the diamond pattern
-
-         for (int i = 0; i <= rows; i++) {
-            string pattern = string.Empty;
-            for (space = 1; space <= rows - i; space++) {
-               pattern += "  ";
-            }
-            for (int j = 0; j <=i; j++) {
-               pattern += "* ";
-            }
-            for (int j = 1; j <= i; j++) {
-               pattern += "* ";
-            }
-            Console.Write ($"\n{pattern}");
+         for(int i = 1; i <= n; i++) {
+            Console.WriteLine (string.Concat (Enumerable.Repeat ("  ", n - i)) + string.Concat (Enumerable.Repeat ("* ", 2 * i - 1)));
          }
-
-
-
-         // build lower half of the diamond pattern
-
-         space = 1;
-         for (int i = rows - 1; i >= 0; i--) {
-            string pattern = string.Empty;
-            for ( space = 1; space <= rows-i; space++) {
-               pattern += "  ";
-            }
-            for (int j = 0; j <=i; j++) {
-               pattern += "* ";
-            }
-            for (int j = 1; j <= i; j++) {
-               pattern += "* ";
-            }
-            Console.Write ($"\n{pattern}");
+         for(int i = n - 1; i >= 1; i--) {
+            Console.WriteLine (string.Concat (Enumerable.Repeat ("  ", n - i)) + string.Concat (Enumerable.Repeat ("* ", 2 * i - 1)));
          }
       }
    }
