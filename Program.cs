db7ecc1fbb2d86ddeba39fda7556898dce131ca4
Sathya K <sathya.k.interns@metamation.com>
2023-08-30 12:04:49 +0530

    Armstrong Assignment T18

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         int n, number, rem, Arm;
+         Console.Write ("Enter a number:");
+          n = int.Parse (Console.ReadLine ());
+          number = n; 
+          rem = 0;
+          Arm = 0;
+
+         while (n > 0) {
+            rem = n % 10;
+            Arm = Arm + (rem * rem * rem);
+            n /= 10;
+         }
+         if (number == Arm) {
+            Console.WriteLine ($"{number} is Armstrong");
+         } else {
+            Console.WriteLine ($"{number} is not Armstrong");
+         }
       }
    }
 }
