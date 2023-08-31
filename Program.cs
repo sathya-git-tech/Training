c6b93398527604e8475c63ca973477a71d4a30b8
Sathya K <sathya.k.interns@metamation.com>
2023-08-31 11:47:25 +0530

    Assignment T21 Swap a given number

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         // swap a given number
+         Console.Write ("Enter a number a:");
+         int a = int.Parse (Console.ReadLine ());
+         Console.Write ("Enter a number b:");
+         int b = int.Parse (Console.ReadLine ());
+
+         int temp = a;
+         a = b;
+         b = temp;
+         Console.WriteLine ($"a={a},b={b}");
       }
    }
 }
