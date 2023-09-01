04d0cb7d6d20d51b81560058b0bd7fbfc98925c1
Sathya K <sathya.k.interns@metamation.com>
2023-09-01 15:23:00 +0530

    changes done T13

--- Program.cs
+++ Program.cs
@@ -9,26 +9,14 @@
          bool digit = password.Any (char.IsDigit);
          bool lower = password.Any (char.IsLower);
          bool upper = password.Any (char.IsUpper);
-         bool specialchar = password.Any (char.IsPunctuation);
-
-         foreach (char i in password ) {  
-               if (char.IsDigit (i)) {
-                  digit = true;
-               }
-               if (char.IsLower (i)) {
-                  lower = true;
-               }
-               if (char.IsUpper (i)) {
-                  upper = true;
-               }
-               if ("!@#$%^&*()-+".Contains (i)) {
-                  specialchar = true;
-               }
-         }
+         bool specialchar = password.Any (c => "!@#$%^&*()-+".Contains (c));
          bool var = length && digit && lower && upper && specialchar;
+
+
          if (var == true) {
             Console.WriteLine ("password is strong");
-         } else {
+         } 
+         else {
             Console.WriteLine ("password is not strong");
             if (password.Length < 6) {
                Console.WriteLine ("password should be at least 6 character");
@@ -42,7 +30,7 @@
             if(!password.Any (char.IsUpper)) {
                Console.WriteLine ("password should contain at least one Uppercase English character");
             }
-            if (!password.Any (char.IsPunctuation)) {
+            if (!password.Any (c => "!@#$%^&*()-+".Contains (c))) {
                Console.WriteLine ("password should contain at least one Special character");
             }
          }
