534c82f7eb62def15c6a78f4ec1e5f7f9767da42
Sathya K <sathya.k.interns@metamation.com>
2023-09-04 10:51:04 +0530

    changes done T19

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.Write ("Enter a number:");
-         int n = int.Parse (Console.ReadLine ());
-         int f = 1;
+﻿using System;
+Console.Write ("Enter a number:");
+int n = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Factorial for " + n + " is " + factorial (n));
 
-         for(int i = 1; i <= n; i++) {
-            f = f * i;
-         }Console.WriteLine ($"factorial of {n} is {f}");
-      }
-   }
+static int factorial(int n) {
+   int f = 1;
+   for (int i = 1; i <= n; i++)
+      f = f * i;
+   return f;
 }
