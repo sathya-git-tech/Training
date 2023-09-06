4e5ae8a3ce1da99981f6052e5a65d343aac6ae37
Sathya K <sathya.k.interns@metamation.com>
2023-09-06 15:26:15 +0530

    changes done T03

--- Program.cs
+++ Program.cs
@@ -2,20 +2,20 @@
    internal class Program {
       static void Main (string[] args) {
          // Fibonacci series
-         Console.Write ("Enter a number n:");
+         Console.Write ("Enter a number n : ");
          int n = int.Parse (Console.ReadLine ());
          int first = 0;
          int second = 1;
          int third = first + second;
+         Console.Write ($"First {n} terms of the fibonacci series : " + first + "," + second);
 
-         string fibonacci = string.Empty;
-         for(int i =2; i <= n-1; i++) {
+         for(int i =2; i < n; i++) {
             third = first + second;
+            Console.Write ("," + third);
             first = second;
             second = third;
-            fibonacci += third;
          }
-         Console.WriteLine ($"Fibonacci of {n} is {fibonacci}"); 
+         Console.WriteLine (); 
 
       }
    }
