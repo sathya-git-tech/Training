b2f676c04626bf354e4ddf03b8fdb1b146a256b5
Sathya K <sathya.k.interns@metamation.com>
2023-09-07 14:49:20 +0530

    Reduced String T14

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("Enter a string:");
+         string input = Console.ReadLine ();
+         char[] character = input.ToArray ();
+         input = input.ToLower ();
+         for (int i = 0; i < input.Length; i++) {
+            string result = string.Empty;
+            if (i == input.Length - 1 || input[i] != input[i + 1]) {
+               result += input[i];
+               Console.Write (result);
+            }
+            else i++;
+         }
       }
    }
 }
