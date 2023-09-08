0e3cc6ae2972359e3971a5c32c74031b1ed4f9c1
Sathya K <sathya.k.interns@metamation.com>
2023-09-08 09:48:51 +0530

    Reduced string T14

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-﻿namespace Training {
+﻿using System.Text;
+namespace Training {
    internal class Program {
       static void Main (string[] args) {
          Console.WriteLine ("Enter a string:");
          string input = Console.ReadLine ();
          char[] character = input.ToArray ();
          input = input.ToLower ();
+         StringBuilder result = new StringBuilder ();
+
          for (int i = 0; i < input.Length; i++) {
-            string result = string.Empty;
             if (i == input.Length - 1 || input[i] != input[i + 1]) {
-               result += input[i];
-               Console.Write (result);
-            }
+               result.Append (input[i]);
+            } 
             else i++;
          }
+         Console.WriteLine (result.Length==0? "Reduced string is 'Empty'" : $"Reduced string is '{result}'");
       }
    }
 }
