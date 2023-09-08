67a55e58ad4ba6cd44ce88ec56b1dfee74a2fe86
Sathya K <sathya.k.interns@metamation.com>
2023-09-08 11:24:08 +0530

    changes done T14

--- Program.cs
+++ Program.cs
@@ -3,18 +3,14 @@ namespace Training {
    internal class Program {
       static void Main (string[] args) {
          Console.WriteLine ("Enter a string:");
-         string input = Console.ReadLine ();
-         char[] character = input.ToArray ();
-         input = input.ToLower ();
-         StringBuilder result = new StringBuilder ();
-
-         for (int i = 0; i < input.Length; i++) {
-            if (i == input.Length - 1 || input[i] != input[i + 1]) {
-               result.Append (input[i]);
-            } 
+         string input = Console.ReadLine ().ToLower ();
+         int l = input.Length;
+         StringBuilder result = new ();
+         for (int i = 0; i < l; i++) {
+            if (i == l - 1 || input[i] != input[i + 1]) result.Append (input[i]);
             else i++;
          }
-         Console.WriteLine (result.Length==0? "Reduced string is 'Empty'" : $"Reduced string is '{result}'");
+         Console.WriteLine (result.Length == 0 ? "Reduced string is 'Empty'" : $"Reduced string is '{result}'");
       }
    }
 }
