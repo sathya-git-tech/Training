2f6f94f82ddabf244f5d6405cf40cd8a43bee75c
Sathya K <sathya.k.interns@metamation.com>
2023-09-11 17:03:36 +0530

    Isogram

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("Enter a word:");
+         string word = Console.ReadLine ();
+         string result = string.Empty;
+         for (int i = 0; i < word.Length; i++) {
+            if (!result.Contains (word[i])) 
+               result += word[i];
+         }
+         Console.WriteLine (word.Length == result.Length ? $"string is Isogram '{result}'" : $"string is not Isogram '{result}'");
+
       }
    }
 }
