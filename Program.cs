5822600a880e4700d5385db0ff524200f55f404f
Sathya K <sathya.k.interns@metamation.com>
2023-09-12 11:35:53 +0530

    Assignment T13

--- Program.cs
+++ Program.cs
@@ -3,14 +3,8 @@
       static void Main (string[] args) {
          Console.WriteLine ("Enter a password:");
          string password = Console.ReadLine ();
-
-
          bool length = password.Length >= 6;
-         bool digit = password.Any (char.IsDigit);
-         bool lower = password.Any (char.IsLower);
-         bool upper = password.Any (char.IsUpper);
-         bool specialchar = password.Any ("!@#$%^&*()-+".Contains);
-
+         bool upper = false; bool lower = false; bool digit = false; bool specialchar = false;
          for (int i = 0; i < password.Length; i++) {
             if (char.IsUpper (password[i])) upper = true;
             if (char.IsLower (password[i])) lower = true;
@@ -18,7 +12,6 @@
             if ("!@#$%^&*()-+".Contains (password[i])) specialchar = true;
          }
          Console.WriteLine (length && digit && upper && lower && specialchar == true ? "password is strong" : "password is not strong");
-
          if (!length) Console.WriteLine ("password should be at least 6 character");
          if (!digit) Console.WriteLine ("password should contain at least one digit");
          if (!lower) Console.WriteLine ("password should contain at least one Lowercase English character");
