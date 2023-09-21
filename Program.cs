27c1ff6853d592a3e2c1d3ea1d793fd12acf7f04
Sathya K <sathya.k.interns@metamation.com>
2023-09-21 16:13:17 +0530

    Abecedarian word program T16

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("enter a words leaving space between them:");
+         string[] words = Console.ReadLine ().Split (' ');
+         string longestWord = FindLongestAbecedarianWord (words);
+         Console.WriteLine (!string.IsNullOrEmpty (longestWord) ? $"The longest abecedarian word is : {longestWord}" : "No abecedarian word");
+      }
+
+      static string FindLongestAbecedarianWord (string[] words) {
+         string longestWord = "";
+         foreach (string eachWord in words) {
+            char[] chars = eachWord.ToCharArray ();
+            Array.Sort (chars);
+            string sortedWord = new string (chars);
+            if (eachWord == sortedWord && eachWord.Length >= longestWord.Length) {
+               longestWord = eachWord;
+            }
+         }
+         return longestWord;
       }
    }
 }
+
