2fdce1e14fb06e38406fe0bb1ae88d05b25d8705
Sathya K <sathya.k.interns@metamation.com>
2023-09-22 17:13:15 +0530

    Swap Indices T22

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.Write ("enter a number leaving space between them:");
+         string[] numbers = Console.ReadLine ().Split (' ');
+         Console.WriteLine ("Original position: " + string.Join (" ", numbers));
+         Console.Write ("Enter the first index to be swapped (0-5): ");
+         int index1 = int.Parse (Console.ReadLine ());
+         Console.Write ("Enter the second index to be swapped (0-5): ");
+         int index2 = int.Parse (Console.ReadLine ());
+         (numbers[index1], numbers[index2]) = (numbers[index2], numbers[index1]);
+         Console.WriteLine ("Swapped result: " + string.Join (" ", numbers));
       }
    }
 }
