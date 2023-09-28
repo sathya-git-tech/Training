3948382116c1483e827310d1601df09eb5846a68
Sathya K <sathya.k.interns@metamation.com>
2023-09-28 16:24:45 +0530

    Display the individual digits of a given number T20

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.Write ("Enter a number: ");
+         string num = Console.ReadLine ();
+         if (double.TryParse (num, out double number)) {
+            string[] parts = num.Split ('.');
+            string integralPart = parts[0];
+            string fractionalPart = parts.Length > 1 ? parts[1] : "";
+            Console.Write ("Integral part: ");
+            foreach (char digit in integralPart) {
+               Console.Write ($"{digit} ");
+            }
+            Console.WriteLine ();
+            if (!string.IsNullOrEmpty (fractionalPart)) {
+               Console.Write ("Fractional part: ");
+               foreach (char digit in fractionalPart) {
+                  Console.Write ($"{digit} ");
+               }
+               Console.WriteLine ();
+            }
+         } else Console.WriteLine ("Invalid input. Please enter a valid number.");
       }
    }
 }
