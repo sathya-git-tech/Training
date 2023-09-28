cd11cf9ca3bd0a49613abc98311658133fbca2dc
Sathya K <sathya.k.interns@metamation.com>
2023-09-28 15:35:52 +0530

    changes done NthArmstrong number T18.1

--- Program.cs
+++ Program.cs
@@ -1,27 +1,31 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.Write ("Enter a number : ");
-         int number = int.Parse (Console.ReadLine ());
-         Console.WriteLine ($"{number}th armstrong number is : " + NthArmstrong (number));
+         Console.Write ("Enter a number: ");
+         if (int.TryParse (Console.ReadLine (), out int number)) {
+            if (number >= 1 && number <= 25) {
+               int nthArmstrong = FindNthArmstrong (number);
+               Console.WriteLine ($"{number}th Armstrong number is: {nthArmstrong}");
+            } else {
+               Console.WriteLine ("Please enter the number between 1 to 25");
+            }
+         } else {
+            Console.WriteLine ("Invalid input. Please provide a valid integer.");
+         }
       }
-      static int NthArmstrong (int n) {
+      static int FindNthArmstrong (int n) {
          int count = 0;
-         for (int i = 1; i <= int.MaxValue; i++) {
+         for (int i = 1; ; i++) {
             int num = i, rem, sum = 0;
-            num = i;
             int length = num.ToString ().Length;
             while (num > 0) {
                rem = num % 10;
                sum += (int)Math.Pow (rem, length);
                num = num / 10;
             }
-            if (i == sum)
-               count++;
-            if (count == n)
-               return i;
+            if (i == sum) count++;
+            if (count == n) return i;
          }
-         return n;
       }
    }
 }
