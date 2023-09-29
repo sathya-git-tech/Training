0755363cc8745b9081def805d06857a1fd70b4e9
Sathya K <sathya.k.interns@metamation.com>
2023-09-29 13:50:14 +0530

    changes done the assignment T22

--- Program.cs
+++ Program.cs
@@ -4,16 +4,18 @@
          int length = 6;
          Random random = new Random ();
          int[] randomNumbers = new int[6];
-         for (int i = 0; i < length; i++) {
-            randomNumbers[i] = random.Next (1, 101);
-         }
+         for (int i = 0; i < length; i++) randomNumbers[i] = random.Next (1, 101);
          Console.WriteLine ("Random Array: " + string.Join (" ", randomNumbers));
          Console.Write ("Enter the first index number: ");
-         int index1 = int.Parse (Console.ReadLine ());
-         Console.Write ("Enter the second index number: ");
-         int index2 = int.Parse (Console.ReadLine ());
-         (randomNumbers[index1], randomNumbers[index2]) = (randomNumbers[index2], randomNumbers[index1]);
-         Console.WriteLine ("Swapped result: " + string.Join (" ", randomNumbers));
+         if (int.TryParse (Console.ReadLine (), out int index1) && index1 >= 0 && index1 < length) {
+            Console.Write ("Enter the second index number: ");
+            if (int.TryParse (Console.ReadLine (), out int index2) && index2 >= 0 && index2 < length) {
+               (randomNumbers[index1], randomNumbers[index2]) = (randomNumbers[index2], randomNumbers[index1]);
+               Console.WriteLine ("Swapped result: " + string.Join (" ", randomNumbers));
+            } 
+            else Console.WriteLine ("Invalid second index number");
+         } 
+         else Console.WriteLine ("Invalid first index number");
       }
    }
 }
