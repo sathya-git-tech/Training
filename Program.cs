69bf2a9b2b105425a0368adb9aa014b196ac7b27
Sathya K <sathya.k.interns@metamation.com>
2023-09-29 12:47:37 +0530

    removed the flower-brace as it is a single statement modified T20

--- Program.cs
+++ Program.cs
@@ -8,18 +8,13 @@
             string integralPart = parts[0];
             string fractionalPart = parts.Length > 1 ? parts[1] : "";
             Console.Write ("Integral part: ");
-            foreach (char digit in integralPart) {
-               Console.Write ($"{digit} ");
-            }
+            foreach (char digit in integralPart) Console.Write ($"{digit} ");
             Console.WriteLine ();
-            if (!string.IsNullOrEmpty (fractionalPart)) {
-               Console.Write ("Fractional part: ");
-               foreach (char digit in fractionalPart) {
-                  Console.Write ($"{digit} ");
-               }
-               Console.WriteLine ();
-            }
-         } else Console.WriteLine ("Invalid input. Please enter a valid number.");
+            if (!string.IsNullOrEmpty (fractionalPart)) Console.Write ("Fractional part: ");
+            foreach (char digit in fractionalPart) Console.Write ($"{digit} ");
+            Console.WriteLine ();
+         } 
+         else Console.WriteLine ("Invalid input. Please enter a valid number.");
       }
    }
 }
