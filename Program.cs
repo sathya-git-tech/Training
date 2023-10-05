85b1ba601e18d84c785e0139cb385f2d25e66d1e
Sathya K <sathya.k.interns@metamation.com>
2023-10-05 14:11:32 +0530

    changes done string permutation T17

--- Program.cs
+++ Program.cs
@@ -4,20 +4,20 @@
          Console.Write ("Enter a string: ");
          string input = Console.ReadLine ().ToUpper ();
          int n = input.Length;
-         permute (input, 0, n - 1);
+         Permute (input, 0, n - 1);
       }
-      static void permute (string input, int l, int r) {
+      static void Permute (string input, int l, int r) {
          if (l == r)
             Console.WriteLine (input);
          else {
             for (int i = l; i <= r; i++) {
-               input = swap (input, l, i);
-               permute (input, l + 1, r);
-               input = swap (input, l, i);
+               input = Swap (input, l, i);
+               Permute (input, l + 1, r);
+               input = Swap (input, l, i);
             }
          }
       }
-      static string swap (string a, int i, int j) {
+      static string Swap (string a, int i, int j) {
          char temp;
          char[] charArray = a.ToCharArray ();
          temp = charArray[i];
