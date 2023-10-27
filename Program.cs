d6b1312eae758c2a64742253e38f1beca9b968db
Sathya K <sathya.k.interns@metamation.com>
2023-10-27 12:14:10 +0530

    changes done A04

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
 // Use the same word list given in the Spelling Bee assignment and build a frequency table with occurrence of all the letters.
 // The output should be a list of a letters with its occurrence count in descending order. Display the first 7 letters to be used as the seed for the Spelling Bee program.
 // --------------------------------------------------------------------------------------------
+using System.Reflection;
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary> Spelling bee frequency </summary
@@ -16,8 +17,10 @@ namespace Training {
       /// <param name="args"></param>
       static void Main (string[] args) {
          Dictionary<char, int> freq = new ();
-         foreach (var ch in File.ReadAllText ("C:\\Users\\sathya.k\\Downloads\\words.txt"))
-            if (ch is >= 'A' and <= 'Z') freq[ch] = freq.ContainsKey (ch) ? freq[ch] + 1 : 1;
+         var path = Assembly.GetExecutingAssembly ().Location;
+         var directoryName = Path.GetDirectoryName (path);
+         foreach (var ch in File.ReadAllText (directoryName + "\\words.txt"))
+            if (ch is >= 'A' and <= 'Z') freq[ch] = freq.TryGetValue (ch, out int value) ? value + 1 : 1;
          foreach (var item in freq.OrderByDescending (x => x.Value).Take (7)) Console.WriteLine ($"{item}");
       }
    }
