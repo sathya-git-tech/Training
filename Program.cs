d710b1620db935d54bc01ba32806590d65fc1514
Sathya K <sathya.k.interns@metamation.com>
2023-11-09 15:55:06 +0530

    changes done T26

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@
 // IndexOutOfRangeException: This exception should be thrown when attempting to access an index that is out of the valid range. 
 // InvalidOperationException: This exception should be thrown when attempting to remove an item that is not found in the list. 
 // --------------------------------------------------------------------------------------------
+using System;
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary> Classes MyList<T> </summary>
@@ -27,75 +28,71 @@ namespace Training {
       /// <summary> Create MyList </summary>
       /// <param name="args"></param>
       static void Main (string[] args) {
-
+         MyList<int> list1 = new ();
+         List<int> list2 = new ();
       }
    }
+   #endregion
    public class MyList<T> {
-      private T[] array;
-      private int count;
-      public MyList () {
-         array = new T[4];
-         count = 0;
-      }
-      public int Count => count;
-      public int Capacity => array.Length;
+      T[] mArray = new T[4];
+      int mCount = 0;
+      public int Count => mCount;
+      public int Capacity => mArray.Length;
       public T this[int index] {
          get {
-            if (index < 0 || index >= count) throw new IndexOutOfRangeException ("Index is out of the valid range.");
-            return array[index];
+            CheckException (index);
+            return mArray[index];
          }
          set {
-            if (index < 0 || index >= count) throw new IndexOutOfRangeException ("Index is out of the valid range.");
-            array[index] = value;
+            CheckException (index);
+            mArray[index] = value;
          }
       }
       /// <summary> Add the element </summary>
       /// <param name="item"></param>
       public void Add (T item) {
-         if (count == array.Length) Array.Resize (ref array, array.Length * 2);
-         array[count] = item;
-         count++;
-      }
-      /// <summary> Remove the element </summary>
-      /// <param name="item"></param>
-      /// <returns></returns>
-      /// <exception cref="InvalidOperationException"></exception>
-      public bool Remove (T item) {
-         int index = Array.IndexOf (array, item, 0, count);
-         if (index < 0 || index >= count) throw new InvalidOperationException ("Item not found in the list.");
-         for (int i = index; i < count - 1; i++) array[i] = array[i + 1];
-         count--;
-         array[count] = default;
-         return true;
+         if (mCount == mArray.Length) Array.Resize (ref mArray, mArray.Length * 2);
+         mArray[mCount++] = item;
       }
       /// <summary> Clear the element </summary>
       public void Clear () {
-         array = new T[4];
-         count = 0;
+         Array.Clear (mArray, 0, mCount);
+         mCount = 0;
       }
       /// <summary> Insert an element </summary>
       /// <param name="index"></param>
       /// <param name="item"></param>
       /// <exception cref="IndexOutOfRangeException"></exception>
       public void Insert (int index, T item) {
-         if (index < 0 || index > count) throw new IndexOutOfRangeException ("Index is out of the valid range.");
-         if (count == array.Length) Array.Resize (ref array, array.Length * 2);
-         for (int i = count; i > index; i--) array[i] = array[i - 1];
-         array[index] = item;
-         count++;
+         if (index < 0 || index > mCount) throw new IndexOutOfRangeException ("Index is out of the valid range.");
+         if (mCount == mArray.Length) Array.Resize (ref mArray, mArray.Length * 2);
+         for (int i = mCount; i > index; i--) mArray[i] = mArray[i - 1];
+         mArray[index] = item;
+         mCount++;
+      }
+      /// <summary> Remove the element </summary>
+      /// <param name="item"></param>
+      /// <returns></returns>
+      /// <exception cref="InvalidOperationException"></exception>
+      public bool Remove (T item) {
+         int index = Array.IndexOf (mArray, item, 0, mCount);
+         if (index < 0 || index >= mCount) throw new InvalidOperationException ("Index is out of the valid range.");
+         for (int i = index; i < mCount - 1; i++) mArray[i] = mArray[i + 1];
+         mArray[--mCount] = default;
+         return true;
       }
       /// <summary> Remove the element at a specific index </summary>
       /// <param name="index"></param>
-      /// <exception cref="IndexOutOfRangeException"></exception>
+      /// <exception cref="ArgumentOutOfRangeException"></exception>
       public void RemoveAt (int index) {
-         if (index < 0 || index >= count) throw new IndexOutOfRangeException ("Index is out of the valid range.");
-         for (int i = index; i < count - 1; i++) array[i] = array[i + 1];
-         count--;
-         array[count] = default;
+         if (index < 0 || index >= mCount) throw new IndexOutOfRangeException ("Index is out of the valid range.");
+         var item = mArray[index];
+         Remove (item);
+      }
+      public bool CheckException (int index) {
+         if (index < 0 || index >= mCount) throw new IndexOutOfRangeException ("Index is out of the valid range.");
+         return true;
       }
    }
-   #endregion 
 }
-#endregion 
-
-
+#endregion
--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -2,13 +2,21 @@ using Training;
 namespace ListTest {
    [TestClass]
    public class UnitTest1 {
+      MyList<int> myList = new ();
+      List<int> list = new ();
       [TestMethod]
       public void TestAdd () {
-         MyList<int> myList = new ();
          myList.Add (10);
          myList.Add (20);
          myList.Add (30);
          myList.Add (40);
+         list.Add (10);
+         list.Add (20);
+         list.Add (30);
+         list.Add (40);
+         Assert.AreEqual (list[0], myList[0]);
+         Assert.AreEqual (myList.Count, list.Count);
+         Assert.AreEqual (myList.Capacity, list.Capacity);
          Assert.AreEqual (4, myList.Count);
          myList.Add (50);
          Assert.AreEqual (8, myList.Capacity);
@@ -20,47 +28,67 @@ namespace ListTest {
          Assert.AreEqual (8, myList.Capacity);
       }
       [TestMethod]
-      public void TestRemove () {
-         MyList<int> myList = new ();
-         myList.Add (10);
-         myList.Add (20);
-         myList.Add (30);
-         myList.Add (40);
-         myList.Remove (10);
-         Assert.AreEqual (3, myList.Count);
-         Assert.ThrowsException<InvalidOperationException> (() => myList.Remove (70));
-      }
-      [TestMethod]
       public void TestClear () {
-         MyList<int> myList = new ();
          myList.Add (10);
          myList.Add (20);
          myList.Add (30);
          myList.Add (40);
          myList.Clear ();
-         Assert.AreEqual (0, myList.Count);
+         list.Add (10);
+         list.Add (20);
+         list.Add (30);
+         list.Add (40);
+         list.Clear ();
+         Assert.AreEqual (myList.Count, list.Count);
       }
       [TestMethod]
       public void TestInsert () {
-         MyList<int> myList = new ();
          myList.Add (10);
          myList.Add (20);
          myList.Add (30);
          myList.Add (40);
          myList.Insert (2, 12);
+         list.Add (10);
+         list.Add (20);
+         list.Add (30);
+         list.Add (40);
+         list.Insert (2, 12);
          Assert.AreEqual (5, myList.Count);
          Assert.AreEqual (8, myList.Capacity);
+         Assert.AreEqual (5, list.Count);
+         Assert.AreEqual (8,list.Capacity);
+         Assert.AreEqual (myList.Count, list.Count);
          Assert.ThrowsException<IndexOutOfRangeException> (() => myList.Insert (7, 12));
       }
       [TestMethod]
+      public void TestRemove () {
+         myList.Add (10);
+         myList.Add (20);
+         myList.Add (30);
+         myList.Add (40);
+         list.Add (10);
+         list.Add (20);
+         list.Add (30);
+         list.Add (40);
+         myList.Remove (10);
+         list.Remove (10);
+         Assert.AreEqual (list.Count, myList.Count);
+         Assert.ThrowsException<InvalidOperationException> (() => myList.Remove (70));
+      }
+      [TestMethod]
       public void TestRemoveAt () {
-         MyList<int> myList = new ();
          myList.Add (10);
          myList.Add (20);
          myList.Add (30);
          myList.Add (40);
          myList.RemoveAt (2);
+         list.Add (10);
+         list.Add (20);
+         list.Add (30);
+         list.Add (40);
+         list.RemoveAt (2);
          Assert.AreEqual (3, myList.Count);
+         Assert.AreEqual (myList.Count, list.Count);
          Assert.ThrowsException<IndexOutOfRangeException> (() => myList.RemoveAt (6));
       }
    }
