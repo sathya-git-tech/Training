7da69a0c6beb9060545523d1c30bf82d28c7b661
Sathya K <sathya.k.interns@metamation.com>
2023-12-22 14:57:44 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
 ﻿// ---------------------------------------------------------------------------------------
 // Training ~ A training program for new interns at Metamation, Batch - July 2023
 // Copyright (c) Metamation India.
-// ---------------------------------------------------------------------------- CUILib ---
+// ---------------------------------------------------------------------------- Training -
 // Program.cs
 // Read the contents of the file input.txt programmatically and change the value of Mango, papaya and Bananana from fruit to vegetable and value of orange from fruit to color.
 // Print the contents in the console and save to a file named out.txt.
 // ---------------------------------------------------------------------------------------
+using System.Reflection;
 namespace Training {
    #region Program -----------------------------------------------------------------------
    /// <summary>Simple basic Assignment</summary>
    internal class Program {
       static void Main (string[] args) {
-         string[] input = File.ReadAllLines ("C:\\Users\\sathya.k\\Downloads\\input.txt");
-         for (int i = 0; i < input.Length; i++) {
+         using var stream = Assembly.GetExecutingAssembly ().GetManifestResourceStream ("Training.data.input.txt");
+         using var reader = new StreamReader (stream);
+         using var writer = new StreamWriter ("..\\..\\..\\data\\output.txt");
+         var input = new List<string> ();
+         while (!reader.EndOfStream)
+            input.Add (reader.ReadLine ());
+         for (int i = 0; i < input.Count; i++) {
             if (input[i].Contains ("Mango") || input[i].Contains ("Bananana") || input[i].Contains ("Papaya")) {
                input[i] = input[i].Replace ("Fruit", "Vegetable");
             }
-            if (input[i].Contains ("Orange")) Console.ForegroundColor = ConsoleColor.Green;
+            if (input[i].Contains ("Orange")) input[i] = input[i].Replace ("Fruit", "Color");
             Console.WriteLine (input[i]);
-            Console.ResetColor ();
+            writer.WriteLine (input[i]);
          }
       }
    }
