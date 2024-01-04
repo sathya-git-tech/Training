a931fb6b96311c949367f7300cfcd22c8df36284
Sathya K <sathya.k.interns@metamation.com>
2024-01-04 18:04:07 +0530

    Assignment : B02

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         int a = -5 + 7 * 2;
+         Console.WriteLine ($"a = {a}");
+         int b = (53 + 2) % 3;
+         Console.WriteLine ($"b = {b}");
+         int c = 46 + -4 * 6 / 11;
+         Console.WriteLine ($"c = {c}");
+         int d = 5 + 40 / 2 * 1 - 5 % 2;
+         Console.WriteLine ($"d = {d}");
       }
    }
 }
