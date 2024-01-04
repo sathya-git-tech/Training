b3043735c4ad01f75157a096b87e47cdafe15b74
Sathya K <sathya.k.interns@metamation.com>
2024-01-04 18:30:40 +0530

    Assignment : B04

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.WriteLine ("Enter the total count of numbers: ");
+         int Total = int.Parse (Console.ReadLine ());
+         float Average; int Sum = 0, Value;
+         for (int i = 0; i < Total; i++) {
+            Console.Write ("Enter a number: ");
+            Value = int.Parse (Console.ReadLine ());
+            Sum += Value;
+         }
+         Average = (float)Sum / Total;
+         Console.WriteLine ("Average: " + Average);
       }
    }
 }
