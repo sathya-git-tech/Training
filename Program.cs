b0daef919b281d0812224e4727a65345ede48dd9
Sathya K <sathya.k.interns@metamation.com>
2024-01-04 18:09:34 +0530

    Assignment : B03

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         Console.Write ("Enter a number1:");
+         int num1 = int.Parse (Console.ReadLine ());
+         Console.Write ("Enter a number2:");
+         int num2 = int.Parse (Console.ReadLine ());
+         Console.WriteLine ($"addition :{num1 + num2}");
+         Console.WriteLine ($"subraction : {num1 - num2}");
+         Console.WriteLine ($"multiplication : {num1 * num2}");
+         Console.WriteLine ($"division : {num1 / num2}");
+         Console.WriteLine ($"modulodivision :{num1 % num2}");
       }
    }
 }
