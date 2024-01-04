76605aad5c1f5d3d912f4c78de2e9a26f7f83497
Sathya K <sathya.k.interns@metamation.com>
2024-01-04 16:42:27 +0530

    Modified the assignment V1

--- Program.cs
+++ Program.cs
@@ -16,27 +16,13 @@ namespace Training {
          using var reader = new StreamReader (stream);
          using var writer = new StreamWriter ("..\\..\\..\\data\\output.txt");
          string output = reader.ReadToEnd ();
-         var lines = output.Split ('\n').ToList ();
-         Dictionary<string, string> replacements = new ();
-         foreach (var i in lines) {
-            lines = i.Split ('=').ToList ();
-            switch (lines[0]) {
-               case "Mango":
-               case "Banana":
-               case "Papaya":
-                  replacements.Add (lines[0], "Vegetable");
-                  break;
-               case "Orange":
-                  replacements.Add (lines[0], "Color");
-                  break;
-               default:
-                  replacements.Add (lines[0], lines[1]);
-                  break;
-            }
-         }
-         foreach (var keyValuePair in replacements) {
-            Console.WriteLine ($"{keyValuePair.Key}={keyValuePair.Value}", $"{keyValuePair.Key}={keyValuePair.Value}");
-            writer.Write ($"{keyValuePair.Key}={keyValuePair.Value}", $"{keyValuePair.Key}={keyValuePair.Value}");
+         var dict = output.Split ('\n').Select (a => a.Split ('=')).ToDictionary (a => a[0], a => a[1]);
+         dict["Mango"] = dict["Banana"] = dict["Papaya"] = "Vegetable";
+         dict["Orange"] = "Color";
+         foreach (var kv in dict) {
+            var s = $"{kv.Key}={kv.Value}";
+            Console.WriteLine (s);
+            writer.WriteLine (s);
          }
       }
    }
