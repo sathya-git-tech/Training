d95eeb5315cb123f46406014dc5bdb5350aba067
Sathya K <sathya.k.interns@metamation.com>
2024-01-08 17:37:40 +0530

    Modified B02

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿/* Write a program to print the result of the following operations: 
-• -5 + 7 * 2 
-• (53 + 2) % 3
-• 46 + -4 * 6 / 11
-• 5 + 40 / 2 * 1 - 5 % 2
-*/
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Write a program to print the result of the following operations: • -5 + 7 * 2 
+// • (53 + 2) % 3 • 46 + -4 * 6 / 11 • 5 + 40 / 2 * 1 - 5 % 2
+// ---------------------------------------------------------------------------------------
 namespace Training {
+   #region Program -----------------------------------------------------------------------
+   /// <summary> Write a program to print the result of the following operations</summary>
    internal class Program {
       static void Main (string[] args) {
          Console.WriteLine (-5 + 7 * 2);
@@ -13,4 +17,5 @@ namespace Training {
          Console.WriteLine (5 + 40 / 2 * 1 - 5 % 2);
       }
    }
+   #endregion 
 }
