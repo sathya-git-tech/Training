81438a49fbfb7b6ff491e2e959c99acef3277694
Sathya K <sathya.k.interns@metamation.com>
2024-01-08 11:09:04 +0530

    Modified B02

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Training {
+﻿/* Write a program to print the result of the following operations: 
+• -5 + 7 * 2 
+• (53 + 2) % 3
+• 46 + -4 * 6 / 11
+• 5 + 40 / 2 * 1 - 5 % 2
+*/
+namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine(-5 + 7 * 2);
+         Console.WriteLine (-5 + 7 * 2);
          Console.WriteLine ((53 + 2) % 3);
          Console.WriteLine (46 + -4 * 6 / 11);
          Console.WriteLine (5 + 40 / 2 * 1 - 5 % 2);
