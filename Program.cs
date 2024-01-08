7510aaec0b3240447a7d12878c3258304ace4346
Sathya K <sathya.k.interns@metamation.com>
2024-01-08 10:02:15 +0530

    Changes done B03

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@
          int num1 = int.Parse (Console.ReadLine ());
          Console.Write ("Enter a number2:");
          int num2 = int.Parse (Console.ReadLine ());
-         Console.WriteLine ($"addition :{num1 + num2}");
-         Console.WriteLine ($"subraction : {num1 - num2}");
-         Console.WriteLine ($"multiplication : {num1 * num2}");
-         Console.WriteLine ($"division : {num1 / num2}");
-         Console.WriteLine ($"modulodivision :{num1 % num2}");
+         Console.WriteLine ($"Addition :{num1 + num2}");
+         Console.WriteLine ($"Subraction : {num1 - num2}");
+         Console.WriteLine ($"Multiplication : {num1 * num2}");
+         Console.WriteLine ($"Division : {num1 / num2}");
+         Console.WriteLine ($"Modulodivision :{num1 % num2}");
       }
    }
 }
