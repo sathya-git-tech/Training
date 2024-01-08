06ff560caf1247a95316728a176756879f457b4b
Sathya K <sathya.k.interns@metamation.com>
2024-01-08 11:25:23 +0530

    Changes done B04

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@ namespace Training {
    internal class Program {
       static void Main (string[] args) {
          int[] total = new int[4];
-         float average; int sum = 0, value;
+         float average; 
+         int sum = 0;
          for (int i = 0; i < total.Length; i++) {
             Console.Write ("Enter a number : ");
-            value = int.Parse (Console.ReadLine ());
+            int value = int.Parse (Console.ReadLine ());
             sum += value;
          }
          average = (float)sum / total.Length;
