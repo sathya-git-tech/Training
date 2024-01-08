f89576823face063e1da564a64dab394a3c1aa73
Sathya K <sathya.k.interns@metamation.com>
2024-01-08 13:07:15 +0530

    Modified B04

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
-﻿// Write a program to calculate and display the average of four numbers entered by the user. 
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Write a program to calculate and display the average of four numbers entered by the user.
+// ---------------------------------------------------------------------------------------
 namespace Training {
+   #region Program -----------------------------------------------------------------------
+   /// <summary>Find the average of four numbers</summary>
    internal class Program {
+      #region Method ------------------------------------------------
+      /// <summary>Getting input from the user</summary>
+      /// <param name="args"></param>
       static void Main (string[] args) {
          int[] total = new int[4];
          float average; 
@@ -11,7 +22,9 @@ namespace Training {
             sum += value;
          }
          average = (float)sum / total.Length;
-         Console.WriteLine ($"Average = {average}");
+         Console.WriteLine ("Average : " + (average));
       }
    }
+   #endregion
 }
+#endregion
