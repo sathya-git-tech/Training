96c7db540be272dddb73e89ec5a72ee420709d6b
Sathya K <sathya.k.interns@metamation.com>
2024-01-08 16:48:07 +0530

    Modified B05

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
-﻿namespace Training {
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Write a program to ask the user for a number and display its multiplication table. 
+// ---------------------------------------------------------------------------------------
+namespace Training {
+   #region Program -----------------------------------------------------------------------
+   /// <summary>Multiplication Table</summary>
    internal class Program {
+      #region Method ------------------------------------------------
+      /// <summary>Gettting input from the user</summary>
+      /// <param name="args">n</param>
       static void Main (string[] args) {
-         Console.Write ("Enter a number:");
+         Console.Write ("Enter a number : ");
          int n = int.Parse (Console.ReadLine ());
          for (int i = 1; i <= 20; i++) {
             Console.WriteLine ($"{i,2}" + " *  " + n + " = " + (i * n));
          }
       }
    }
+   #endregion
 }
+#endregion
