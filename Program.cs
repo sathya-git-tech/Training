3e1530f0041256f2d60560661ad0e8d8c6f7342d
Sathya K <sathya.k.interns@metamation.com>
2024-01-09 18:18:43 +0530

    Assignment : B18

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿namespace Training {
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Create a C# program to ask the user for 5 numbers,
+// store them in an array and show them in reverse order.  
+// ---------------------------------------------------------------------------------------
+namespace Training {
+   #region Program --------------------------------------------------------------------------------
+   /// <summary>Reverse order</summary>
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         int[] n = new int[5];
+         for (; ; ) {
+            Console.Write ("Enter a number : ");
+            int value = int.Parse (Console.ReadLine ());
+            int remainder = 0;
+            while (value > 0) {
+               remainder = ((remainder * 10) + value % 10);
+               value /= 10;
+            }
+            Console.WriteLine ($"{remainder}");
+         }
       }
    }
 }
+#endregion
