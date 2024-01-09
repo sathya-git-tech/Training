b6a7b4990dd8ac4d2a5c4350100f2b9ac9f26ba5
Sathya K <sathya.k.interns@metamation.com>
2024-01-09 12:58:08 +0530

    Changes done B09

--- Program.cs
+++ Program.cs
@@ -3,25 +3,26 @@
 // Copyright (c) Metamation India.
 // ---------------------------------------------------------------------------- Training -
 // Program.cs
-// Write a program to ask the user for an undetermined amount of numbers (until 0 is entered) and display their sum (using do...while). 
+// Write a program to ask the user for an undetermined amount of
+// Numbers (until 0 is entered) and display their sum (using do...while). 
 // ---------------------------------------------------------------------------------------
 namespace Training {
    #region Program -----------------------------------------------------------------------
-   /// <summary>A program to ask the user for an undetermined of numbers and display their sum</summary>
+   /// <summary>Ask the user for an undetermined of number and display their sum</summary>
    internal class Program {
       #region Method ------------------------------------------------
       /// <summary>Getting input from the user</summary>
       /// <param name="args">number</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Enter a number : ");
-         int number = int.Parse (Console.ReadLine ());
-         int total = 0;
-         do {
-            total += (number % 10);
-            number /= 10;
+         for (; ; ) {
+            int sum = 0, number;
+            do {
+               Console.Write ("Enter a number : ");
+               number = Convert.ToInt32 (Console.ReadLine ());
+               sum += number;
+            } while (number != 0);
+            Console.WriteLine ("Sum of the numbers : " + sum);
          }
-         while (number > 0);
-         Console.WriteLine ($"Sum is : {total}");
       }
    }
    #endregion 
