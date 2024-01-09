7fbd882b54f98494f132610c76514af8311faf1d
Sathya K <sathya.k.interns@metamation.com>
2024-01-09 14:36:18 +0530

    Assignment : B07

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿namespace Training {
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Create a program to ask the user for a number "x" and display 10 * x.
+// It must repeat until the user enters 0 (using "while"). 
+// ---------------------------------------------------------------------------------------
+namespace Training {
+   #region Program -----------------------------------------------------------------------
+   /// <summary>The program to ask the user for a number "x" and display 10 * x</summary>
    internal class Program {
+      #region Method ------------------------------------------------
+      /// <summary>Getting input from the user</summary>
+      /// <param name="args">x</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         while (true) {
+            Console.Write ("Enter a number : ");
+            int x = int.Parse (Console.ReadLine ());
+            if (x == 0) break;
+            int result = 10 * x;
+            Console.WriteLine ($"10 * {x} = {result}");
+         }
       }
    }
+   #endregion 
 }
+#endregion
