f4df893ecb4d87ef00b4759d06872864d96eaf26
Sathya K <sathya.k.interns@metamation.com>
2024-01-12 12:51:56 +0530

    Assignment : B22

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
-﻿namespace Training {
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Write a program to ask the user for the number and
+// Calculate the absolute value of a given number(x).
+// If the number is positive, its absolute value is the same number (x).
+// If it is negative, its absolute value is -x. 
+// ---------------------------------------------------------------------------------------
+
+namespace Training {
+   #region Program --------------------------------------------------------------------------------
+   /// <summary>Find the absolute value</summary>
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         for (; ; ) {
+            Console.Write ("Enter a number : ");
+            int number = int.Parse (Console.ReadLine ());
+            number = Math.Abs (number);
+            Console.WriteLine ($"Absolute value is : {number}");
+         }
       }
    }
+   #endregion 
 }
