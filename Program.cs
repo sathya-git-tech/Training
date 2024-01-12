9d83f865c87441a01effed22833762e263a456c5
Sathya K <sathya.k.interns@metamation.com>
2024-01-12 11:28:23 +0530

    Modified B21

--- Program.cs
+++ Program.cs
@@ -5,34 +5,36 @@
 // Program.cs
 // Write a program to print the even numbers from 2 to 50, except 20.
 // You need to write this program in 3 different ways:
-// Incrementing 2 in each step (use "continue" to skip 16).
+// Incrementing 2 in each step (use "continue" to skip 20).
 // Incrementing 1 in each step (use "continue").
 // Endless loop using "break" & "continue".
 // ---------------------------------------------------------------------------------------
 
 namespace Training {
-   #region Program --------------------------------------------------------------------------------
+   #region Program ---------------------------------------------------------------------
    /// <summary>Print even numbers 2 to 50 (except 20)</summary>
    internal class Program {
       static void Main (string[] args) {
-         int i = 2;
-         // Print the even numbers from 2 to 50, except 20.
-         Console.WriteLine ("Print the even numbers from 2 to 50, except 20.");
-         for (i = 2; i <= 50; i += 2) {
+         Console.WriteLine ("Incrementing 2 in each step (use \"continue\" to skip 20) .");
+         for (int i = 2; i <= 50; i += 2) {
             if (i == 20) continue;
             Console.WriteLine (i);
          }
-         // Incrementing 2 in each step (use "Continue" to skip 16).
-         Console.WriteLine ("Incrementing 2 in each step");
-         for (i = 2; i <= 50; i += 2) {
-            if (i == 16) continue;
-            Console.WriteLine (i);
-         }
-         // Incrementing 1 in each step (use "continue").
-         Console.WriteLine ("Incrementing 1 in each step");
-         for (i = 2; i <= 50; i++) {
+         Console.WriteLine ("Incrementing 1 in each step use \"continue\" .");
+         for (int i = 2; i <= 50; i++) {
             if (i == 20) continue;
-            Console.WriteLine (i);
+            if (i % 2 == 0) Console.WriteLine (i);
+         }
+         int j = 2;
+         Console.WriteLine ("Endless loop using \"break\" & \"continue\" .");
+         while (true) {
+            if (j > 50) break;
+            if (j == 20) {
+               j += 2;
+               continue;
+            }
+            Console.WriteLine (j);
+            j += 2;
          }
       }
    }
