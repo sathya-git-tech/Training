31cf939a11a5cd2793e4ff8677a3041b15f1a9f3
Sathya K <sathya.k.interns@metamation.com>
2024-01-17 10:26:19 +0530

    Assignment : B23

--- Program.cs
+++ Program.cs
@@ -1,7 +1,35 @@
-﻿namespace Training {
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Write a program to ask the user for two numbers and display their division.
+// Errors must be catches using "try..catch". 
+// ---------------------------------------------------------------------------------------
+
+namespace Training {
+   #region Program --------------------------------------------------------------------------------
+   /// <summary>Errors must be catches using "try..catch"</summary>
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         for (; ; ) {
+            try {
+               Console.Write ("Enter the first number: ");
+               double num1 = double.Parse (Console.ReadLine ());
+               Console.Write ("Enter the second number: ");
+               double num2 = double.Parse (Console.ReadLine ());
+               if (num2 != 0) {
+                  double result = num1 / num2;
+                  Console.WriteLine ($"{num1} / {num2} = {result}");
+               }
+               if (num2 == 0) throw new DivideByZeroException ();
+            } catch (FormatException) {
+               Console.Write ("Invalid input. Please enter a valid number\n");
+            } catch (DivideByZeroException) {
+               Console.Write ("Division by zero is not allowed. Please enter a non-zero value in the second number.\n");
+            }
+         }
       }
    }
+   #endregion
 }
