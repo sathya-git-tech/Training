d98c4fbc49121e874a8a4b9fe2dbbb28a0f059dd
Sathya K <sathya.k.interns@metamation.com>
2024-01-29 11:43:52 +0530

    Changes done B28

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿namespace Training {
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new interns at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------- Training -
+// Program.cs
+// Write a program to ask the user for enter two persons
+// Name and date of birth and display them in console.For that,
+// You must create the struct for a Person, the fields for Person will be: 
+// string Name Date Birthday Date is a another struct, the fields for Date is: int Day 
+// int Month int Year. 
+// ---------------------------------------------------------------------------------------
+
+namespace Training {
+   #region Program --------------------------------------------------------------------------------
+   /// <summary>Create the struct for a person, the fields for Name and Birthday Date</summary>
    internal class Program {
       public struct Date {
          public int Day;
@@ -10,28 +24,30 @@
          public string Name;
          public Date Birthday;
       }
+
       static void Main (string[] args) {
-         Person p1, p2;
-         Console.Write ("Enter a name (with intial) : ");
-         p1.Name = Console.ReadLine ();
-         Console.Write ("Enter a Birythday day : ");
-         p1.Birthday.Day = int.Parse (Console.ReadLine ());
-         Console.Write ("Enter a Birthday Month : ");
-         p1.Birthday.Month = int.Parse (Console.ReadLine ());
-         Console.Write ("Enter a Birthday year : ");
-         p1.Birthday.Year = int.Parse (Console.ReadLine ());
-         Console.Write ($"\nName : {p1.Name}\nBirthday day : {p1.Birthday.Day}\nBirthday Month : {p1.Birthday.Month}\nBirthday year : {p1.Birthday.Year}\n");
-         Console.WriteLine ();
-         Console.Write ("Enter a name : ");
-         p2.Name = Console.ReadLine ();
-         Console.Write ("Enter a Birythday day : ");
-         p2.Birthday.Day = int.Parse (Console.ReadLine ());
-         Console.Write ("Enter a Birthday Month : ");
-         p2.Birthday.Month = int.Parse (Console.ReadLine ());
-         Console.Write ("Enter a Birthday year : ");
-         p2.Birthday.Year = int.Parse (Console.ReadLine ());
-         Console.Write ($"\nName : {p2.Name}\nBirthday day : {p2.Birthday.Day}\nBirthday Month : {p2.Birthday.Month}\nBirthday year : {p2.Birthday.Year}");
-         Console.WriteLine ();
+         for (; ; ) {
+            Person p1, p2;
+            Console.Write ("Enter a name (with intial) : ");
+            p1.Name = Console.ReadLine ();
+            Console.Write ("Enter a Birythday Day : ");
+            p1.Birthday.Day = int.Parse (Console.ReadLine ());
+            Console.Write ("Enter a Birthday Month : ");
+            p1.Birthday.Month = int.Parse (Console.ReadLine ());
+            Console.Write ("Enter a Birthday Year : ");
+            p1.Birthday.Year = int.Parse (Console.ReadLine ());
+            Console.Write ("Enter a name : ");
+            p2.Name = Console.ReadLine ();
+            Console.Write ("Enter a Birythday Day : ");
+            p2.Birthday.Day = int.Parse (Console.ReadLine ());
+            Console.Write ("Enter a Birthday Month : ");
+            p2.Birthday.Month = int.Parse (Console.ReadLine ());
+            Console.Write ("Enter a Birthday Year : ");
+            p2.Birthday.Year = int.Parse (Console.ReadLine ());
+            Console.Write ($"{p1.Name} : {p1.Birthday.Day}-{p1.Birthday.Month}-{p1.Birthday.Year}\n");
+            Console.Write ($"{p2.Name} : {p2.Birthday.Day}-{p2.Birthday.Month}-{p2.Birthday.Year}\n");
+         }
       }
    }
+   #endregion
 }
